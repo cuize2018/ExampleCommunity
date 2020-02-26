4c49b87c1aea366020ed5d0e99edddf64601b48d
cuize2018 <847290082@qq.com>
2020-02-26 14:50:49 +0800

    添加SpringMVC的拦截器

--- src/main/java/com/xk/community/interceptor/WebConfig.java
+++ src/main/java/com/xk/community/interceptor/WebConfig.java
@@ -8,6 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 //@EnableWebMvc 删除该注解，否则无法加载css文件
+//使用该注解时应覆写addResource方法，但是没有写，所以无法加载样式；
+//不使用该注解时使用默认的资源路径
 public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
